39fb120c85fa37e1ce4441f27af4f151ec4d8443
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-05-05 10:19:23 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/jcu/cp3406/guesstheceleb/MainActivity.java
@@ -0,0 +1,20 @@
+package au.edu.jcu.cp3406.guesstheceleb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.AssetManager;
+import android.os.Bundle;
+
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
