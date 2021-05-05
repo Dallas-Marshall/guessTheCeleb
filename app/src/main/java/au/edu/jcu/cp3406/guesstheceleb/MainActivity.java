0926145347d673300fa59b8f0d0a835e2ef799db
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-05-05 10:20:51 +1000

    Add AssetManager to MainActivity

--- app/src/main/java/au/edu/jcu/cp3406/guesstheceleb/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/guesstheceleb/MainActivity.java
@@ -15,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        AssetManager manager = getAssets();
+        try {
+            String[] names = manager.list("celebs");
+            System.out.println(Arrays.toString(names));
+        } catch (IOException e) {
+            System.out.println("failed to get names");
+        }
     }
 }
