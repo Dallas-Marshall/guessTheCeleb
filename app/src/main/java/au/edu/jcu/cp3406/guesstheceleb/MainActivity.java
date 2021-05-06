551db6fd84721bf9b6c94967b3b210a71f7e489e
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-05-06 14:54:49 +1000

    Replace placeholder code with code for GameFragment

--- app/src/main/java/au/edu/jcu/cp3406/guesstheceleb/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/guesstheceleb/MainActivity.java
@@ -1,12 +1,8 @@
 package au.edu.jcu.cp3406.guesstheceleb;
 
-import android.content.res.AssetManager;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     ImageManager imageManager;
@@ -15,22 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        AssetManager manager = getAssets();
-//        try {
-//            String[] names = manager.list("celebs");
-//            System.out.println(Arrays.toString(names));
-        imageManager = new ImageManager(this.getAssets(), "celebs");
-        ImageView imageView = findViewById(R.id.image);
-//
-//            InputStream stream = manager.open("celebs/" + names[0]);
-//            Bitmap bitmap = BitmapFactory.decodeStream(stream);
 
-        imageView.setImageBitmap(imageManager.get(0));
-//    } catch(
-//    IOException e)
-
-//        {
-//            System.out.println("failed to get names");
-//        }
     }
 }
